86d910394c2eaf16acc6b6773978895c96d8f230
luffy12 <dvamshikrishna007@gmail.com>
2022-02-20 17:54:55 -0500

    game changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import {debounce} from 'lodash';
+const MOLE_SCORE = 1;
+const MOLE = 3;
 
-function App() {
+export default function App() {
+  const [playing, setPlaying] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const [score, setScore] = useState(0);
+  const [hitPosition, setHitPosition] = useState();
+  const [intervalId, setIntervalId] = useState();
+  const [stop, setStop] = useState(false);
+  const [won, setWon] = useState(false);
+  const [color, setColor] = useState(false);
+
+  useEffect(() => {
+    if (playing) {
+      let interval = setInterval(() => {
+        let randomMole = Math.floor(Math.random() * MOLE) + 1;
+        console.log(randomMole);
+        setHitPosition(randomMole);
+        setColor(false);
+
+      }, 1000);
+      setIntervalId(interval)
+    }
+  }, [playing]);
+
+  const startGame = () => {
+    setScore(0);
+    setPlaying(true);
+    setFinished(false);
+    setStop(true);
+    setColor(false);
+    setWon(false);
+  };
+  const stopGame = () => {
+    setPlaying(false);
+    setFinished(true);
+    setStop(false);
+    clearInterval(intervalId);
+  }
+
+  useEffect(() => {
+    if (score == 5) {
+      setPlaying(false);
+      setFinished(true);
+      setWon(true);
+      clearInterval(intervalId);
+    }
+  }, [score])
+  const whack = (id) => {
+    console.log("clicked")
+    if(id == hitPosition){
+      setScore(score + 1);
+      setColor(true);
+    }
+    else{
+      setScore(score - 1);
+      setColor(false);
+    }
+  };
+  const moleStyle = (moleHit) => {
+    if (moleHit) {
+      return {
+        backgroundColor: color?"red":"yellow",
+        height: "60px",
+        width: "60px",
+        borderRadius: "30px",
+        marginLeft: "10px"
+      }
+    }
+    else {
+      return {
+        backgroundColor: "black",
+        height: "60px",
+        width: "60px",
+        borderRadius: "30px",
+        marginLeft: "10px"
+      }
+    }
+
+  };
+  const molesStyle = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      marginTop: "40px"
+  }
+
+  const debouncedClick = debounce((mole)=>{
+    whack(mole);
+  },1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {finished ? (
+          <div>
+            <h2>{won ? "You have won" : "Game finished"}</h2>
+            <h2>Score: {score}</h2>
+            <button onClick={startGame}>play again</button>
+          </div>
+        ) : (
+            <div>
+              <h1 data-testid="heading">Whack-a-mole</h1>
+              <h2>Score: {score}</h2>
+              <button data-testid="Startbutton" onClick={stop ? stopGame : startGame}>{stop ? "Stop" : "Start"}</button>
+              <div
+                style={molesStyle}
+              >
+                {Array.from({ length: MOLE }, (_, i) => i + 1).map((mole) => (
+                  <div
+                    key={mole}
+                    onClick={() => debouncedClick(mole)}
+                    style={
+                      moleStyle(hitPosition == mole)
+                    }
+                  ></div>
+                ))}
+              </div>
+            </div>
+          )}
+      </div>
     </div>
   );
 }
 
-export default App;
+
+// export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 
+afterEach(cleanup);
 test('renders learn react link', () => {
+  // const root =document.createElement("div");
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByTestId('heading');
+  const Startbutton = screen.getByTestId('Startbutton');
+
+  expect(heading).toHaveTextContent("Whack-a-mole");
+  expect(Startbutton).toHaveTextContent("Start");
 });
+
